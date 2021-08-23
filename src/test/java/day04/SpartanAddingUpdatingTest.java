1c4e8a86350304182cde56f85dc958e6260fdd2d
AJan <wakarjan@hotmail.com>
2021-08-22 23:28:18 -0400

    Day04- Posting new data Starter

---
+++ src/test/java/day04/SpartanAddingUpdatingTest.java
@@ -0,0 +1,32 @@
+package day04;
+
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+
+public class SpartanAddingUpdatingTest {
+
+    @BeforeAll
+    public static void setUp() {
+        baseURI = "http://35.170.61.191:8000";
+        basePath = "/api";
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        setUp();
+    }
+
+
+
+
+
+}
