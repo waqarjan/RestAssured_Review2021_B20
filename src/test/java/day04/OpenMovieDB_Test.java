34ddeb1d7d9067e8c1c25d9208f3ef7ea1b5eaf9
AJan <wakarjan@hotmail.com>
2021-08-22 22:20:07 -0400

    Day04- OpenMovie Database API Test with LOG for request and response --John Wick!

--- src/test/java/day04/OpenMovieDB_Test.java
+++ src/test/java/day04/OpenMovieDB_Test.java
@@ -30,19 +30,36 @@ public class OpenMovieDB_Test {
     public void testMovie() {
 
         given()
-            .queryParam("apikey", "af37c803")
-            .queryParam("t", "The Orville").
-        when()
-                .get(baseURI). prettyPeek().
-        then()
+                .queryParam("apikey", "af37c803")
+                .queryParam("t", "The Orville").
+                when()
+                .get(baseURI).prettyPeek().
+                then()
                 .statusCode(is(200))
                 .contentType(ContentType.JSON)
                 .body("Title", is("The Orville"))
                 .body("Ratings[0].Source", is("Internet Movie Database"))
-
-
-                ;
-
+        ;
     }
 
+
+        @DisplayName("Getting the log of request and response")
+        @Test
+        public void testSendingRequestAndGetTheLog(){
+            given()
+                    .queryParam("apikey", "af37c803")
+                    .queryParam("t", "John Wick")
+                    //logging the request should be in given section
+                    .log().all().
+            when()
+                    .get(baseURI).
+            then()
+                    .log().all()
+                    //logging the response should be in then section
+                    .statusCode(is(200))
+                    .contentType(ContentType.JSON)
+                    .body("Plot", containsString("ex-hit-man"))
+                    .body("Ratings[1].Source", is("Rotten Tomatoes"))
+                    ;
+    }
 }
