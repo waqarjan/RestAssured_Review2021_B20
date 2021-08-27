fe4f5b50b3ccfdf09c778e3dd84a2fe047f114e9
AJan <wakarjan@hotmail.com>
2021-08-27 02:20:45 -0400

    Day05- added dependency for JavaFaker

---
+++ src/test/java/day05/TestingOrderingJunit5.java
@@ -0,0 +1,18 @@
+package day05;
+
+// by default the test is running on alphabetical order
+// or the test method name
+
+import org.junit.jupiter.api.* ;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.MethodOrderer.* ;
+
+// these are all available option for ordering your tests
+//@TestMethodOrder(OrderAnnotation.class)
+//@TestMethodOrder(Random.class)
+//@TestMethodOrder(MethodName.class) // default options
+
+public class TestingOrderingJunit5 {
+
+
+}
