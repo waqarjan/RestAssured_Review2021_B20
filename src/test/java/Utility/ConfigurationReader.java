d6b22469e808d9cbdb3500e7ea0755e87576fb29
AJan <wakarjan@hotmail.com>
2021-08-26 20:38:49 -0400

    Day05- Added Utility Package for Configuration and DB_Utility

---
+++ src/test/java/Utility/ConfigurationReader.java
@@ -0,0 +1,27 @@
+package Utility;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.io.*;
+import java.util.*;
+
+public class ConfigurationReader {
+
+
+    private static Properties properties = new Properties();
+    static{
+        try {
+            FileInputStream file = new FileInputStream("configuration.properties");
+            properties.load(file);
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Properties File not found");
+        }
+    }
+    public static String getProperty(String keyWord){
+        return properties.getProperty(keyWord);
+    }
+
+
+}
