8b5c1abeb3a5c65a22702a217192b6d0fc00af89
AJan <wakarjan@hotmail.com>
2021-09-07 01:34:25 -0400

    Day07: HR ORDS Database Get 1 Region

---
+++ src/test/java/day07/HR_ORDS_Test.java
@@ -0,0 +1,46 @@
+package day07;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import pojo.Region;
+import java.util.List;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class HR_ORDS_Test {
+
+    @BeforeAll
+    public static void setUp(){
+        baseURI = "http://35.170.61.191:1000";
+        basePath = "/ords/hr" ;
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        reset();
+    }
+
+    @DisplayName("Testing /regions/region_id")
+    @Test
+    public void testThirdRegionIsAsia(){
+
+        given()
+                .pathParam("region_id", 3)
+                .log().all().
+        when()
+                .get("/regions/{region_id}").
+        then()
+                .log().all()
+                .assertThat()
+                .statusCode(is(200))
+                .contentType(ContentType.JSON)
+                .body("region_name",is("Asia"))
+                ;
+    }
+
+
+
+
+}
